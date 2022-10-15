55d6b39fb0e7b8f6d7c14d276bcff0b1ee217681
YourLocalHomeInvader <109906500+YourLocalHomeInvader@users.noreply.github.com>
2022-10-15 09:19:54 -0400

    Update main.cs

--- main.cs
+++ main.cs
@@ -66,7 +66,7 @@ ModAPI.Register(
 			{
 				OriginalItem = ModAPI.FindSpawnable("Pistol"),
 				NameOverride = "<color=green> Dream Beat",
-				DescriptionOverride = "\n<color=green>**Disable Collision before using**",
+				DescriptionOverride = "\n<color=green>**Disable Collision before using** Press F to play the music",
 				CategoryOverride = ModAPI.FindCategory("<color=green> Dream"), 
 				ThumbnailOverride = ModAPI.LoadSprite("sprites/Dream Beat.png"), 
                                NameToOrderByOverride = "a",
@@ -98,7 +98,7 @@ ModAPI.Register(
 			{
 				OriginalItem = ModAPI.FindSpawnable("Pistol"),
 				NameOverride = "<color=green> Dream Boss",
-				DescriptionOverride = "\n<color=yellow>**Disable Collision before using**",
+				DescriptionOverride = "\n<color=yellow>**Disable Collision before using**Press F to Use it",
 				CategoryOverride = ModAPI.FindCategory("<color=green> Dream"), 
 				ThumbnailOverride = ModAPI.LoadSprite("sprites/Dream Boss.png"), 
                                NameToOrderByOverride = "a",
@@ -110,16 +110,25 @@ ModAPI.Register(
 						Cartridge customCartridge = ModAPI.FindCartridge("9mm");
 						customCartridge.name = "DMusic";
 						firearm.Cartridge = customCartridge;
-						customCartridge.Damage = 0f;
-						customCartridge.StartSpeed *= 0f; 
+						customCartridge.Damage = 10f;
+						customCartridge.StartSpeed *= 2f; 
 						customCartridge.PenetrationRandomAngleMultiplier *= 0f; 
 						customCartridge.Recoil *= 0f; 
-						customCartridge.ImpactForce *= 0f; 
+						customCartridge.ImpactForce *= 0f;
 						firearm.ShotSounds = new AudioClip[]
 						{
-							ModAPI.LoadSound("sounds/Boss Music.mp3"),
+						ModAPI.LoadSound("sounds/Pickle.mp3"),
+						ModAPI.LoadSound("sounds/breathe air.mp3"),
+						ModAPI.LoadSound("sounds/AUGH.mp3"),
+						ModAPI.LoadSound("sounds/Scream.mp3"),
 						};
 						Instance.FixColliders();
+						Instance.AddComponent<UseEventTrigger>().Action = () =>
+						{
+          					ModAPI.CreateParticleEffect("BigZap", Instance.transform.position);
+          					ModAPI.CreateParticleEffect("Explosion", Instance.transform.position);
+                        		Instance.GetComponent<GeneratorBehaviour>().TargetCharge = 150000;
+						};
 					}
 				}
 			);
